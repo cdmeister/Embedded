ccb8fcecea00611ef5d664192193e55d20a3265f
Moeiz Riaz <moeizr@umich.edu>
2018-12-11 14:31:25 -0800

    clean up and using macros

--- projects/blinky/blinky.c
+++ projects/blinky/blinky.c
@@ -49,24 +49,30 @@ int main(void)
   // Set mode of all pins as digital output
   // 00 = digital input       01 = digital output
   // 10 = alternate function  11 = analog (default)
-  GPIOD->MODER &= ~(0xFF<<24); /* Clear mode bits */
-  GPIOD->MODER |= 85UL<<24;/* LED 5-8 are on GPIOD Pins 12-15 */
+  GPIOD->MODER &= ~(GPIO_MODER_MODE12 | GPIO_MODER_MODE13
+                   | GPIO_MODER_MODE14 | GPIO_MODER_MODE15); /* Clear mode bits */
+  GPIOD->MODER |= (GPIO_MODER_MODE12_0 | GPIO_MODER_MODE13_0
+                   | GPIO_MODER_MODE14_0 | GPIO_MODER_MODE15_0);/* LED 5-8 are on GPIOD Pins 12-15 */
 
   // Set output type of all pins as push-pull
   // 0 = push-pull (default)
   // 1 = open-drain
-  GPIOD->OTYPER &= ~(0xF<<3); /*Configure as output open-drain */
+  GPIOD->OTYPER &= ~(GPIO_OTYPER_OT12 | GPIO_OTYPER_OT13 |
+                   GPIO_OTYPER_OT14 | GPIO_OTYPER_OT15); /*Configure as output open-drain */
 
   // Set output speed of all pins as high
   // 00 = Low speed           01 = Medium speed
   // 10 = Fast speed          11 = High speed
-  GPIOD->OSPEEDR &=~(0xFF<<3); /* Configure as high speed */
-  GPIOD->OSPEEDR |= (0xFF<<3);
+  GPIOD->OSPEEDR &=~(GPIO_OSPEEDR_OSPEED12 | GPIO_OSPEEDR_OSPEED13 | /* Configure as high speed */
+                    GPIO_OSPEEDR_OSPEED14 | GPIO_OSPEEDR_OSPEED15); /* Configure as high speed */
+  GPIOD->OSPEEDR |= (GPIO_OSPEEDR_OSPEED12 | GPIO_OSPEEDR_OSPEED13 |
+                    GPIO_OSPEEDR_OSPEED14 | GPIO_OSPEEDR_OSPEED15);
 
   // Set all pins as no pull-up, no pull-down
   // 00 = no pull-up, no pull-down    01 = pull-up
   // 10 = pull-down,                  11 = reserved
-  GPIOD->PUPDR &= ~(0xFF<<3); /*no pul-up, no pull-down*/
+  GPIOD->PUPDR &= ~(GPIO_PUPDR_PUPD12 | GPIO_PUPDR_PUPD13
+                  | GPIO_PUPDR_PUPD14 | GPIO_PUPDR_PUPD15); /*no pul-up, no pull-down*/
 
   unsigned int delay =0;
 
