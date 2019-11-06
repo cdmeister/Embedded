9189f14428f4cd41e83d055df9e5ffa63f2bb5e8
Moeiz Riaz <moeizr@umich.edu>
2019-11-05 20:51:06 -0800

    adding complete vector table and linker defines and weak alias

--- projects/bare_metal/startup.c
+++ projects/bare_metal/startup.c
@@ -1,6 +1,6 @@
 #include "stdlib.h"
-#include "stdint.h"
 #include "string.h"
+#include "linker_defines.h"
 
 typedef unsigned long uint32_t;
 /* Memory and peripheral start address(common to all STM32 MCUs) */
@@ -48,47 +48,25 @@ int main(void);
 void delay ( uint32_t count);
 void _start(void);
 
-/* Minimal vector table */
-void (* const vector_table[])(void) __attribute__((section(".isr_vector"))) = {
-  (void (*)(void))SRAM_END, /*initial stack pointer*/
-  _start /* main as Reset_Handler*/
-};
-
-
-/* Begin address for the initialization values of the .data section
- * defined in the linker script
- * https://stackoverflow.com/questions/8398755/access-symbols-defined-in-the-linker-script-by-application
-*/
-
-extern uint32_t _sidata;
-extern uint32_t _sdata;
-extern uint32_t _edata;
-
-extern uint32_t _sbss;
-extern uint32_t _ebss;
-
-extern uint32_t _start_heap;
-extern uint32_t _end_heap;
 
 volatile uint32_t __attribute__((used)) dataVar = 0x3F;
 volatile uint32_t __attribute__((used)) bssVar;
 const uint32_t val = 1111;
 const char msg[] = "Hello World";
 
-inline void _initialize_data(uint32_t * flash_begin, uint32_t * data_begin,
+void _initialize_data(uint32_t * flash_begin, uint32_t * data_begin,
                               uint32_t * data_end){
   uint32_t * p = data_begin;
   while(p<data_end) *p++ = *flash_begin++;
 
 }
 
-inline void _initialize_bss(uint32_t * bss_begin, uint32_t * bss_end){
+void _initialize_bss(uint32_t * bss_begin, uint32_t * bss_end){
   uint32_t * p = bss_begin;
   while(p<bss_end) *p++ = 0;
 
 }
 
-/* THIS IS BROKEN */
 void * _sbrk(uint32_t incr){
   static uint32_t * heap = NULL;
   void * old_heap = heap;
@@ -107,22 +85,25 @@ void * _sbrk(uint32_t incr){
   return old_heap;
 }
 
-void __attribute__((noreturn,weak)) _start(void){
+void Reset_Handler(void){
   _initialize_data(&_sidata,&_sdata,&_edata);
   _initialize_bss(&_sbss,&_ebss);
   main();
   for(;;);
 }
 
+
 int main() {
   const uint32_t * p = &val;
-  char * heapMsg =(char *)malloc(sizeof(char)* (strlen(msg)));
+  const uint32_t size_string = strlen(msg);
+  char * heapMsg =(char *)malloc(sizeof(char)* size_string);
   strcpy(heapMsg,msg);
   /* Enable clock on GPIOA peripheral */
   *RCC_AHB1ENR |= (0x1<<0x3U);
   /* Configure the PA5 as output pull-up */
   *GPIOD_MODER |= GPIO_MODER_MODE12_0; /* Sets MODER[11:10] = 0x1*/
   bssVar = 0x3F;
+  free(heapMsg);
   while(bssVar == dataVar) {
     /* CORTEX M4F only support single precision hence the X.XXF */
     *GPIOD_ODR ^= 0x1000;
