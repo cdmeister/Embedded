9039439f14a77c7dc555122e1d5f3a0a1d1220e0
Moeiz Riaz <moeizr@umich.edu>
2020-02-05 23:19:58 -0800

    compile fixes

--- projects/bare_metal/stm32f407xx.h
+++ projects/bare_metal/stm32f407xx.h
@@ -70,15 +70,15 @@ typedef struct
 
 typedef struct
 {
-  __IO uint32_t MODER;    /*!< GPIO port mode register,               Address offset: 0x00      */
-  __IO uint32_t OTYPER;   /*!< GPIO port output type register,        Address offset: 0x04      */
-  __IO uint32_t OSPEEDR;  /*!< GPIO port output speed register,       Address offset: 0x08      */
-  __IO uint32_t PUPDR;    /*!< GPIO port pull-up/pull-down register,  Address offset: 0x0C      */
-  __IO uint32_t IDR;      /*!< GPIO port input data register,         Address offset: 0x10      */
-  __IO uint32_t ODR;      /*!< GPIO port output data register,        Address offset: 0x14      */
-  __IO uint32_t BSRR;     /*!< GPIO port bit set/reset register,      Address offset: 0x18      */
-  __IO uint32_t LCKR;     /*!< GPIO port configuration lock register, Address offset: 0x1C      */
-  __IO uint32_t AFR[2];   /*!< GPIO alternate function registers,     Address offset: 0x20-0x24 */
+  volatile uint32_t MODER;    /*!< GPIO port mode register,               Address offset: 0x00      */
+  volatile uint32_t OTYPER;   /*!< GPIO port output type register,        Address offset: 0x04      */
+  volatile uint32_t OSPEEDR;  /*!< GPIO port output speed register,       Address offset: 0x08      */
+  volatile uint32_t PUPDR;    /*!< GPIO port pull-up/pull-down register,  Address offset: 0x0C      */
+  volatile uint32_t IDR;      /*!< GPIO port input data register,         Address offset: 0x10      */
+  volatile uint32_t ODR;      /*!< GPIO port output data register,        Address offset: 0x14      */
+  volatile uint32_t BSRR;     /*!< GPIO port bit set/reset register,      Address offset: 0x18      */
+  volatile uint32_t LCKR;     /*!< GPIO port configuration lock register, Address offset: 0x1C      */
+  volatile uint32_t AFR[2];   /*!< GPIO alternate function registers,     Address offset: 0x20-0x24 */
 } GPIO_TypeDef;
 
 
