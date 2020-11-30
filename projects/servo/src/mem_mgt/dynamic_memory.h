aa6db3577e3dc832c7b056845558efda6d265d8b
Moeiz Riaz <moeizr@umich.edu>
2020-11-30 13:51:54 -0500

    timer and pwm initial setup

---
+++ projects/servo/src/mem_mgt/dynamic_memory.h
@@ -0,0 +1,19 @@
+/******************************************************************************
+*
+* This file contains functions that help facilitate dyanmic memory allocation
+*
+*******************************************************************************/
+
+#ifndef _DYNAMIC_MEM_ALLOCATION_
+#define _DYNAMIC_MEM_ALLOCATION_
+
+#include "stdint.h"
+#include "stdlib.h"
+#include "linker_defines.h"
+
+void * _sbrk(uint32_t incr);
+
+
+
+
+#endif
