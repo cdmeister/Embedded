a8337f57700a3df216c9b89d3080a3cb0001d1fc
Moeiz Riaz <moeizr@umich.edu>
2021-01-17 19:51:54 -0500

    removing rtos folder

--- projects/rtos/README.md
+++
@@ -1,1 +0,0 @@
-Using bare_metal approch to make a simple RTOS as a learning tool
--- projects/rtos/scheduler.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SCHEDULER_
-#define __SCHEDULER_
-
-#include "stm32f407xx.h"
-/* Calls the PENDSV ISR */
-#define schedule() SCB->ICSR |= SCB_ICSR_PENDSVSET_Msk;
-
-
-#endif /*__SCHEDULER_*/
